4db709728121a97cde21e9af61aa7604610f3ed1
lipeng-gzmu <1826427954@qq.com>
2021-05-18 15:21:55 +0800

    李鹏的第2次提交

--- src/Test02.java
+++ src/Test02.java
@@ -1,5 +1,9 @@
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("i'm test02");
+
+    }
+    public void add(){
+        String add = "在student02分支创建了一个方法";
     }
 }
